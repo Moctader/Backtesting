4a3706e16821f34a1e1821e6faeb8d8628d0fc0d
Md Golam Moctader <golam.moctader@gmail.com>
2024-11-03 13:21:39 +0200

    refactor for all the signal

--- signals.py
+++ signals.py
@@ -69,11 +69,23 @@ class BinaryPlusExitSignal(BaseSignal):
 
 
 class MulticlassSignal(BaseSignal):
+    def __init__(self, current_price, buy_signal, sell_signal, exit_signal, quartiles):
+        super().__init__(current_price, buy_signal, sell_signal, exit_signal)
+        self.quartiles = quartiles
+
+    def generate_signal(self, predicted_high):
+        if self.current_price < self.quartiles[0]:
+            return "Buy"
+        elif self.current_price > self.quartiles[2]:
+            return "Sell"
+        else:
+            return "Hold"
+
     def generate_buy_signal(self, predicted_high):
-        return self.buy_signal(self.current_price, predicted_high)
+        return self.generate_signal(predicted_high) == "Buy"
 
     def generate_sell_signal(self, buy_price):
-        return self.sell_signal(self.current_price, buy_price) if buy_price else False
+        return self.generate_signal(buy_price) == "Sell"
 
     def generate_exit_signal(self, buy_price, predicted_high):
-        return self.exit_signal(self.current_price, buy_price, predicted_high) if buy_price else False
+        return self.generate_signal(predicted_high) == "Hold"
